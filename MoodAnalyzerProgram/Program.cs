ef74ec72beeb2cc6f210cc85356f6080aac91f77
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-27 15:14:06 +0530

    [Anurag] Add: Mood Analyzer

---
+++ MoodAnalyzerProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyzer Program");
+        }
+    }
+}
